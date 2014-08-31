fe8bb998a08994ad66d3e386d50b44d9682cbfd5
Stephan <catsby@catsby-edb.netia.firma>
2014-08-31 18:14:51 +0200

    Added images folder and basic functionality

--- listen.rb
+++ listen.rb
@@ -1,34 +1,36 @@
 require 'listen'
 require 'nokogiri'
 current_directory = Dir.pwd
-
-listener = Listen.to(current_directory) do |modified, added, removed|
+listening_directory = current_directory + "/images"
+ 
+listener = Listen.to(listening_directory) do |modified, added, removed|
   puts "modified absolute path: #{modified}"
   puts "added absolute path: #{added}"
   puts "removed absolute path: #{removed}"
-  	if added.empty? == false
-		file = File.open("test2.html")
-		doc = Nokogiri::HTML(file)
+ 
+  if added.empty? == false
 
-		body = doc.at_css "body"   # Finner body, og setter inn en ny div først
-		new_div = Nokogiri::XML::Node.new 'div id="box"' , doc
-		body.first_element_child.before(new_div)
-		File.open("test2.html", "w") {|file| file.write(doc)}
+  	filename_full = added.to_s
+	filename = filename_full.split('images/').last.split('"').first
+   	filename = filename.to_s
+  	fileaddress = "images/" + filename
+  	puts fileaddress
 
-		fil = File.open("test2.html")  # Laster filen på nytt, for å med den nye diven
-		doc = Nokogiri::HTML(fil)
-
-		div_to_change = doc.at_css 'div'  # Setter inn a href inni den første (den nye) diven
-		new_link = Nokogiri::XML::Node.new 'a href="http://www.freak.no"', doc
-		new_link.content = added
-		div_to_change.add_child(new_link)
-
-		File.open("test2.html", "w") {|file| file.write(doc.to_html)}	
-	end
+    File.open("index.html", "r+") do |file|
+      doc = Nokogiri::HTML(file)
+      file.rewind
+      body = doc.at_css "body" # Finner body, og setter inn en ny div først
+      new_div = Nokogiri::XML::Node.new 'div id="box"', doc
+      new_link = Nokogiri::XML::Node.new 'a href="images/' + filename + '"\'', doc 
+      new_link.content = filename
+      new_div.add_child(new_link)
+      body.first_element_child.before(new_div)
+      file.write(doc.to_html)
+    end
+  end
 end
 
+
+ 
 listener.start # not blocking
 sleep
-
-
-
